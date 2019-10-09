473defe246561fadd20c80d94f47f405c83323d5
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-10-09 09:49:42 +0300

    Load JQuery before Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 // Sending Contact Message Function
-
-
-$(document).ready(function(){
+$(document).ready(() => { 
+    //open modal on clicking upload
+    alert(1)
+    $("#uploadTrigger").click(() => {
+        $("#uploadModal").modal("show")
+    })
     $("form").submit(function(event){
         eventPreventDefault();
         var name = $("#name").val();
