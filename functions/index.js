267ad425ab61a763b9580684e8ae66f457cf5c84
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-10-11 09:50:25 +0300

    Second deploy

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
